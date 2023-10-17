b7d0485318c412c2fa07c222c2b644306615be27
filip-mirazchiyski <124933131+filip-mirazchiyski@users.noreply.github.com>
2023-10-17 14:55:09 +0300

    Add files via upload

---
+++ 04_functions/01_lab/06_calculate_rectangle_area.py
@@ -0,0 +1,7 @@
+def calculate_rectangle_area(length, width):
+    area = length * width
+    return area
+
+result = calculate_rectangle_area(length = int(input()), width = int(input()))
+
+print(result)
