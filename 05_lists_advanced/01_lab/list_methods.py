49f0bbbe091940e85ca2e72a66b67e8fd63f091b
filip-mirazchiyski <124933131+filip-mirazchiyski@users.noreply.github.com>
2023-10-18 17:28:14 +0300

    Add files via upload

---
+++ 05_lists_advanced/01_lab/list_methods.py
@@ -0,0 +1,19 @@
+my_list = [1, 2, 3]
+my_list.append(4)
+print(my_list)
+
+my_list = [1, 2, 3]
+my_list.insert(0, 22) #index, value
+print(my_list)
+
+my_list = [1, 2, 3]
+my_list.remove(2)
+print(my_list)
+
+my_list = [1, 2, 3]
+my_list.pop()
+print(my_list)
+
+my_list = [1, 2, 3]
+my_list.extend([4])
+print(my_list)
