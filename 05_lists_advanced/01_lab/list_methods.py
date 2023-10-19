78330ee4d09f3854ba1403a5ce9e96cbac72dc56
filip-mirazchiyski <124933131+filip-mirazchiyski@users.noreply.github.com>
2023-10-19 13:00:19 +0300

    Add files via upload

--- 05_lists_advanced/01_lab/list_methods.py
+++ 05_lists_advanced/01_lab/list_methods.py
@@ -17,3 +17,10 @@ print(my_list)
 my_list = [1, 2, 3]
 my_list.extend([4])
 print(my_list)
+
+fruits = ["apple", "banana", "orange", "banana", "banana"]
+number_of_bananas = fruits.count("banana")
+element_index = fruits.index("orange") # always returns the first index it finds it at
+element_index = fruits.index("orange", 1) # specifying an index to start the search from
+print(number_of_bananas)
+print(element_index)
