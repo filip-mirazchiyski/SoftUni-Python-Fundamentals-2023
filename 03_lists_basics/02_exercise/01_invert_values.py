aa939a314b01d44a9a8b7bf2534f213ece340cc5
filip-mirazchiyski <124933131+filip-mirazchiyski@users.noreply.github.com>
2023-10-19 12:52:56 +0300

    Add files via upload

---
+++ 03_lists_basics/02_exercise/01_invert_values.py
@@ -0,0 +1,12 @@
+string = input()
+my_list = (list(string.split(" "))) # We split the integer and add in a list
+int_list = [int(i) for i in my_list] # We turn the list values from Strings to Integers using List comprehension
+new_list = [] # We create a new empty list
+
+for number in int_list: # We iterate through the int_list and check each number if it is + or -
+    if number > 0:
+        new_list.append(-abs(number)) # If number is positive, reverse its value and add to new list
+    else:
+        new_list.append(abs(number)) # If number is negative, reverse its value and add to new list
+
+print(new_list)
