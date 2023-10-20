baa0fce1a231863d25f4afc5bbed6a22436e1b7e
filip-mirazchiyski <124933131+filip-mirazchiyski@users.noreply.github.com>
2023-10-20 14:02:02 +0300

    Add files via upload

---
+++ 05_lists_advanced/01_lab/list_map.py
@@ -0,0 +1,8 @@
+numbers = list(map(int, input().split(", ")))
+print(numbers)
+
+names = ["ivan", "pesho", "gosho"]
+
+capitalized_names = map(str.capitalize, names)
+
+print(list(capitalized_names))
