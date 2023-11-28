4982019405d81c44341d56b0809b4e5b31976490
filip-mirazchiyski <124933131+filip-mirazchiyski@users.noreply.github.com>
2023-11-28 12:17:23 +0200

    Add files via upload

---
+++ 09_text_processing/02_exercise/07_string_explosion.py
@@ -0,0 +1,17 @@
+some_string = input()
+final_string = ""
+strength = 0
+
+for index in range(len(some_string)):
+    # We have explosion
+    if strength > 0 and some_string[index] != ">":
+        strength -= 1
+    # We have explosion mark
+    elif some_string[index] == ">":
+        final_string += some_string[index]
+        strength += int(some_string[index + 1])
+    # We have no explosion -> normal character
+    else:
+        final_string += some_string[index]
+
+print(final_string)
